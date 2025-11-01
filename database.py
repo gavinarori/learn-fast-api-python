9623a276f0e302fdba780596db46eb3e5bc26cd5
gavinarori <arorigavin@gmail.com>
2025-11-01 13:52:47 +0300

    first commit

--- database.py
+++ database.py
@@ -1,8 +1,18 @@
-# database.py
+
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-MONGO_URL = "mongodb+srv://arorigavincode_db_user:aNl2mDqmRwPb5ZBX@cluster0.vehurzz.mongodb.net/?retryWrites=true&w=majority"
+# Load .env file
+load_dotenv()
 
+# Get MongoDB URL from environment variables
+MONGO_URL = os.getenv("MONGO_URL")
+
+if not MONGO_URL:
+    raise ValueError("❌ MONGO_URL is not set in .env file")
+
+# Connect to MongoDB
 client = AsyncIOMotorClient(MONGO_URL, tlsAllowInvalidCertificates=True)
 db = client["fastapi_auth_db"]
 users_collection = db["users"]
